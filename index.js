9542e74649dd0934094d7c67c4adaa886db46652
colorizerde <colorizerdev@gmail.com>
2025-02-27 16:08:51 +0200

    Update index.js6

--- index.js
+++ index.js
@@ -3,25 +3,6 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const multer = require("multer");
 const session = require("express-session");
-const NotificationModel = require("./models/NotificationModel");
-const forumRouter = require("./router/forumRoutes");
-const userRouter = require("./router/UsersRouter");
-const MessagesProjectRoutes = require("./router/messagesProjectRoutes");
-const friendshipRoutes = require("./router/friendshipRoutes");
-const notificationRouter = require("./router/notificationRoutes");
-const chatRoutes = require("./router/chatRoutes");
-const jobRoutes = require("./router/jobRoutes");
-const profileRouter = require("./router/profileRouter");
-const ProjectRoutes = require("./router/ProjectRoutes");
-const contactRoutes = require("./router/contactRoutes");
-const adminMessageRoutes = require("./router/adminMessageRoutes");
-const adminDashboardRoutes = require("./router/adminDashboardRoutes");
-const adminStatisticsRoutes = require("./router/adminStatisticsRoutes");
-const adminSiteStatsRoutes = require("./router/adminSiteStatsRoutes");
-const adminRolesPermissionsRoutes = require("./router/adminRolesPermissionsRoutes");
-const adminForumSettingsRoutes = require("./router/adminForumSettingsRoutes");
-const adminJobProjectSettingsRoutes = require("./router/adminJobProjectSettingsRoutes");
-const GlobalRoleController = require("./controllers/GlobalRoleController");
 
 const app = express();
 
@@ -67,73 +48,35 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.resolve(__dirname, "uploads")));
 
-// Middleware لحساب unreadCount وتمريره إلى جميع الصفحات
-app.use(async (req, res, next) => {
-  if (req.session && req.session.userId) {
-    try {
-      const unreadCount = await NotificationModel.getUnreadCount(req.session.userId);
-      res.locals.unreadCount = unreadCount || 0;
-    } catch (err) {
-      console.error("حدث خطأ أثناء حساب unreadCount:", err);
-    }
-  } else {
-    res.locals.unreadCount = 0;
-  }
-  next();
+// مسار الصفحة الرئيسية
+app.get("/", (req, res) => {
+  res.render("login"); // يمكنك تغييره إلى res.render("login") إذا أردت
 });
-
-// تطبيق Middleware عالمي للتحقق من الدور
-app.use(GlobalRoleController.setGlobalRole);
-
-// دمج الراوترات
-app.use("/", userRouter);
-app.use(friendshipRoutes);
-app.use(notificationRouter);
-app.use("/forum", forumRouter);
-app.use("/", chatRoutes);
-app.use("/", jobRoutes);
-app.use("/", profileRouter);
-app.use("/projects", ProjectRoutes);
-app.use("/", MessagesProjectRoutes);
-app.use("/", contactRoutes);
-app.use("/admin", adminMessageRoutes);
-app.use("/admin", adminDashboardRoutes);
-app.use("/admin", adminStatisticsRoutes);
-app.use("/admin", adminSiteStatsRoutes);
-app.use("/admin", adminRolesPermissionsRoutes);
-app.use("/admin", adminForumSettingsRoutes);
-app.use("/admin", adminJobProjectSettingsRoutes);
-app.use("/", require("./router/GlobalRoleRouter"));
 
 // مسارات ثابتة للصفحات
-app.get("/", (req, res) => {
-  res.render("login");
+app.get("/about", (req, res) => {
+  res.render("about", {
+    unreadCount: res.locals.unreadCount || 0,
+    userId: res.locals.userId || null,
+    isAdmin: res.locals.isAdmin || false,
+  });
 });
 
-app.get("/about", (req, res) =>
-  res.render("about", {
-    unreadCount: res.locals.unreadCount,
-    userId: res.locals.userId,
-    isAdmin: res.locals.isAdmin,
-  })
-);
-
-app.get("/privacy", (req, res) =>
+app.get("/privacy", (req, res) => {
   res.render("privacy", {
-    unreadCount: res.locals.unreadCount,
-    userId: res.locals.userId,
-    isAdmin: res.locals.isAdmin,
-  })
-);
+    unreadCount: res.locals.unreadCount || 0,
+    userId: res.locals.userId || null,
+    isAdmin: res.locals.isAdmin || false,
+  });
+});
 
-// مسار مستقل لـ /ProjectSpace
 app.get("/ProjectSpace", (req, res) => {
   res.render("ProjectSpace", {
     errorMessage: null,
     successMessage: null,
-    userId: res.locals.userId,
-    isAdmin: res.locals.isAdmin,
-    unreadCount: res.locals.unreadCount,
+    userId: res.locals.userId || null,
+    isAdmin: res.locals.isAdmin || false,
+    unreadCount: res.locals.unreadCount || 0,
   });
 });
 
