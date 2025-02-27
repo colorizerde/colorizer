34b497c7e09a614ddeee0c8fd250cf99e233647a
colorizerde <colorizerdev@gmail.com>
2025-02-27 16:15:52 +0200

    Update index.js7

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const multer = require("multer");
 const session = require("express-session");
-
+const userRouter = require("./router/UsersRouter");
 const app = express();
 
 // إعداد التسجيل للأخطاء
@@ -48,38 +48,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.resolve(__dirname, "uploads")));
 
-// مسار الصفحة الرئيسية
+// مسار اختبار بسيط
 app.get("/", (req, res) => {
-  res.render("login"); // يمكنك تغييره إلى res.render("login") إذا أردت
+  res.render("login");
 });
-
-// مسارات ثابتة للصفحات
-app.get("/about", (req, res) => {
-  res.render("about", {
-    unreadCount: res.locals.unreadCount || 0,
-    userId: res.locals.userId || null,
-    isAdmin: res.locals.isAdmin || false,
-  });
-});
-
-app.get("/privacy", (req, res) => {
-  res.render("privacy", {
-    unreadCount: res.locals.unreadCount || 0,
-    userId: res.locals.userId || null,
-    isAdmin: res.locals.isAdmin || false,
-  });
-});
-
-app.get("/ProjectSpace", (req, res) => {
-  res.render("ProjectSpace", {
-    errorMessage: null,
-    successMessage: null,
-    userId: res.locals.userId || null,
-    isAdmin: res.locals.isAdmin || false,
-    unreadCount: res.locals.unreadCount || 0,
-  });
-});
-
+app.use("/", userRouter);
 // التقاط الأخطاء العامة
 app.use((err, req, res, next) => {
   console.error("Error occurred:", err.stack);
